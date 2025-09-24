696696ba5d6ba18c32be89bacd347bb351d39e75
Pranav Waykar <PWaykar@nextsmartmove.ai>
2025-09-24 17:16:23 +0530

    fix(storybook): set manager theme with base='light' to ensure brand shows

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,9 +1,9 @@
 import { addons } from '@storybook/manager-api';
-import { create, themes } from '@storybook/theming';
+import { create } from '@storybook/theming';
 
 addons.setConfig({
   theme: create({
-    ...themes.light,
+    base: 'light',
     brandTitle: 'cmp-library',
     brandUrl: 'https://github.com/pranavwaykar/nsm-component-library',
     brandImage: '/vite.svg'
