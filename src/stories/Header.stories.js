f32228d1acab62a8eeabe92bd9f34198f667103d
Pranav Waykar <PWaykar@nextsmartmove.ai>
2025-09-24 16:46:47 +0530

    chore: initial commit (Vite React + Storybook)

---
+++ src/stories/Header.stories.js
@@ -0,0 +1,34 @@
+import { fn } from 'storybook/test';
+
+import { Header } from './Header';
+
+export default {
+  title: 'Example/Header',
+  component: Header,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+    docs: {
+      description: {
+        component: 'Explain what Header is for, usage guidance, accessibility notes.'
+      }
+    }
+  },
+  args: {
+    onLogin: fn(),
+    onLogout: fn(),
+    onCreateAccount: fn(),
+  },
+};
+
+export const LoggedIn = {
+  args: {
+    user: {
+      name: 'Jane Doe',
+    },
+  },
+};
+
+export const LoggedOut = {};
